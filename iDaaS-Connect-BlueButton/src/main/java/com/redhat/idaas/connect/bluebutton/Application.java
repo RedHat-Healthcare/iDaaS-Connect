475e36bf5aa9f82885d599c77f5ad85f163d60fa
Alan Scott <alscott@redhat.com>
2021-05-28 22:43:00 -0500

    Series of enhancements to support ConfigProperties better and introduce metrics

--- iDaaS-Connect-BlueButton/src/main/java/com/redhat/idaas/connect/bluebutton/Application.java
+++ iDaaS-Connect-BlueButton/src/main/java/com/redhat/idaas/connect/bluebutton/Application.java
@@ -21,7 +21,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(OAuthToken.class)
+@EnableConfigurationProperties(ConfigProperties.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
