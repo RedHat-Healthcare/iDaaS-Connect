a7b70f974a531e3652b733c6992aa0fd5cd24c66
Alan Scott <alscott@redhat.com>
2022-01-27 11:07:44 -0600

    Updates to iDaaS-Connect-ThirdParty to support iOT device requests

--- iDaaS-Connect-ThirdParty/src/main/java/com/redhat/idaas/connect/thirdparty/CamelConfiguration.java
+++ iDaaS-Connect-ThirdParty/src/main/java/com/redhat/idaas/connect/thirdparty/CamelConfiguration.java
@@ -29,6 +29,7 @@ import org.apache.camel.component.kafka.KafkaEndpoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jms.connection.JmsTransactionManager;
 import org.springframework.stereotype.Component;
@@ -70,16 +71,23 @@ public class CamelConfiguration extends RouteBuilder {
     KafkaEndpoint kafkaEndpoint = new KafkaEndpoint();
     return kafkaEndpoint;
   }
- /* @Bean
-  private KafkaComponent kafkaComponent(KafkaEndpoint kafkaEndpoint){
-    KafkaComponent kafka = new KafkaComponent();
-    return kafka;
-  }*/
+
 
   private String getKafkaTopicUri(String topic) {
     return "kafka:" + topic +
             "?brokers=" +
             config.getKafkaBrokers();
+  }
+  @Bean
+  ServletRegistrationBean camelServlet() {
+    // use a @Bean to register the Camel servlet which we need to do
+    // because we want to use the camel-servlet component for the Camel REST service
+    ServletRegistrationBean mapping = new ServletRegistrationBean();
+    mapping.setName("CamelServlet");
+    mapping.setLoadOnStartup(1);
+    mapping.setServlet(new CamelHttpTransportServlet());
+    mapping.addUrlMappings("/idaas/*");
+    return mapping;
   }
 
   @Override
